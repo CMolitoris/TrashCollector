ce3868a622b3b244eb3ef4e8c361ce0e44004118
TGWolf <60626007+sfodd1@users.noreply.github.com>
2021-09-27 15:12:15 -0400

    Create/register form created for employee account

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,6 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.apps import apps
+from django.urls.base import reverse
+from .models import Employee
 
 # Create your views here.
 
@@ -11,3 +15,15 @@ def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
+
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        zip_from_form = request.POST.get('zip_code')
+        new_employee = Employee(name=name_from_form,zip=zip_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')
